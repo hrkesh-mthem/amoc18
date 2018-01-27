85287013ccac69ee68f24701bcd3e0cc99e1dc28
yashraj143 <yashrajkhatri764@gmail.com>
2018-01-27 12:43:16 +0530

    after login user_page added but required modify

---
+++ user_page.php
@@ -0,0 +1,139 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "photograph";
+session_start();
+$conn = mysqli_connect($servername, $username, $password);
+mysqli_select_db($conn,$database);
+
+//$name =$_SESSION["USERNAME"];
+$name = 'yash';
+$sql = "select profile from profile where name ='$name' ";
+$result = mysqli_query($conn,$sql);
+
+if (!$result) {
+    printf("Error: %s\n", mysqli_error($conn));
+    exit();
+}
+
+$row = mysqli_fetch_array($result);
+
+
+$sqll = "select cover from cover where name ='$name' ";
+$result1 = mysqli_query($conn,$sqll);
+
+if (!$result1) {
+    printf("Error: %s\n", mysqli_error($conn));
+    exit();
+}
+
+$row1 = mysqli_fetch_array($result1);
+
+	
+	
+	
+	
+
+
+//$image = $row['image'];
+
+//$image_src = "upload/".$image;
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+body {
+    font-family: "Times new Roman", sans-serif;
+}
+
+.sidenav {
+    height: 100%;
+    width: 200px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #66b3ff;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+.sidenav a {
+    padding: 6px 10px 6px 16px;
+   
+    font-size: 25px;
+    color: #111;
+    display: block;
+}
+
+.sidenav a:hover {
+    color:#ff0066;
+}
+
+.main {
+    margin-left: 200px; /* Same as the width of the sidenav */
+    font-size: 28px; /* Increased text to enable scrolling */
+    padding: 0px 0px;
+	border: solid #ff0066;
+	border-style: double;
+    border-width: 10px;
+	  position: relative;
+}
+
+#id1 {
+    border-radius: 90%;
+	
+}
+.cover{
+	width: 100%;
+    max-width: 100%;
+    height: 500px;
+	 display: block;
+	
+	
+}
+.text_block {
+    position: absolute;
+    bottom: 5px;
+    right: 10px;
+    background-color: none;
+    color: white;
+    padding-left: 20px;
+    padding-right: 20px;
+}
+</style>
+</head>
+<body>
+
+<div class="sidenav">
+
+  <br>
+  <img src="<?=$row[0]?>" width="175" height="175" alt ="Set Profile Pic" id="id1">  
+  <br>
+  <br>
+  <a href="user_about.php">About</a>
+  <a href="#services">My Gallary</a>
+  <a href="#contact">Contact</a>
+</div>
+
+<div class="main">
+  
+ <img src="<?=$row1[0]?>" alt="Nature" class="cover">
+ <div class="text_block"> 
+    <h4>Nature</h4>
+    <p>What a beautiful sunrise</p>
+  </div>
+  
+  
+  
+
+</div>
+     
+</body>
+</html> 
